bfce670f477c30933fc49c334b5d51df796da6fc
Zefyro <71092443+Zefyro@users.noreply.github.com>
2023-09-14 10:54:56 +0300

    Deleted mention timestamp
    Deleted mentions will now have their creation timestamp sent in the DailyEagle thread

--- EagleBot/Main.cs
+++ EagleBot/Main.cs
@@ -28,6 +28,16 @@ public partial class EagleBot {
             if (timeSinceLastMention < TimeSpan.FromHours(18))
                 await e.Message.RespondAsync("Too early! :angry:\n*||You can ping Eagle again in 18 hours.||*");
         };
+        Client.MessageDeleted += async (s, e) => {
+            // Check if the message was sent in the Daily Eagle thread
+            if (e.Message.Channel.Id != Config.DailyEagleThread)
+                return;
+            // Check if the message mentions EagleEye621
+            if (!e.Message.MentionedUsers.Any(user => user.Id == /* EagleEye621 User Id */ 589604959352520725))
+                return;
+            // send the timestamp of a deleted message
+            await e.Channel.SendMessageAsync($"A message pinging EagleEye621 by {e.Message.Author.Mention} <t:{e.Message.CreationTimestamp.ToUnixTimeMilliseconds}:R> was deleted.");
+        };
         await Task.Delay(-1);
     }
 }
